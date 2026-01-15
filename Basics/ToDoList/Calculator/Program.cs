89666a9b09da809832ab49a681b3454e1a83a984
Mihael Knezevic <mihaelk270@gmail.com>
2026-01-15 12:31:15 +0100

    Initial commit: C# projects setup

---
+++ Basics/ToDoList/Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello!");
+
+Console.WriteLine("Input the first number:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do? \r\n [A]dd numbers\r\n[S]ubtract numbers\r\n[M]ultiply numbers");
+string op = Console.ReadLine();
+
+int result = 0;
+
+switch (op.ToUpper()) 
+{
+    case "A":
+        result = a + b;
+        break;
+    case "S":
+        result = a - b;
+        break;
+    case "M":
+        result = a * b;
+        break;
+    default:
+        Console.WriteLine("Nepoznata operacija");
+        return;
+}
+
+
+Console.WriteLine($"Rezultat: {result}");
+
+
+Console.ReadKey();
+
+
+
---
+++ Basics/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,18 @@
+﻿
+bool IsLong(string input)
+{
+    return input.Length > 10;
+}
+
+var userChoice = Console.ReadLine();
+bool isLong = IsLong(userChoice);
+
+Console.WriteLine(isLong);
+
+
+
+
+
+
+
+Console.ReadKey();
