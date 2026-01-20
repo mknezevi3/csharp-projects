4fae521287315bd564ea67ad817ad0265de2a279
Mihael Knezevic <mihaelk270@gmail.com>
2026-01-20 19:27:22 +0100

    Add initial todo list(WIP)

--- Basics/ToDoList/ToDoList/Program.cs
+++ Basics/ToDoList/ToDoList/Program.cs
@@ -1,14 +1,99 @@
 ﻿
-bool IsLong(string input)
+using System.Diagnostics.Tracing;
+
+List<string> toDo = new List<string>();
+
+string userInput;
+
+
+static void addToDo(List<string> toDo)
 {
-    return input.Length > 10;
+
+    string description;
+
+    Console.WriteLine("Enter your description");
+
+do
+{
+    
+        do
+        {
+            description = Console.ReadLine();
+            if (toDo.Contains(description)
+                { 
+            Console.WriteLine("Description must be unique!");
+                }
+        } while(toDo.Contains(description));
+
+
+
+    if (string.IsNullOrWhiteSpace(description))
+    {
+        Console.WriteLine("Description can not be empty,try again.");
+    }
+
+} while (string.IsNullOrWhiteSpace(description));
+
+
+toDo.Add(description);
+
+
 }
 
-var userChoice = Console.ReadLine();
-bool isLong = IsLong(userChoice);
 
-Console.WriteLine(isLong);
+static void seeToDos(List<string> toDo) 
+{
+    int i = 1;
+    foreach (string ToDo in toDo) 
+    {
+        
+        Console.WriteLine( i + "." + $"{ToDo}");
+        i++;
+    }
+}
 
+
+
+
+
+
+
+
+
+
+do
+{
+    Console.WriteLine("What do you want to do? \r\n[S]ee all todos\r\n[A]dd a todo\r\n[R]emove a todo\r\n[E]xit");
+
+
+    do
+    {
+        userInput = Console.ReadLine();
+        userInput = userInput.ToUpper();
+
+        if (userInput != "S" && userInput != "A" && userInput != "R" && userInput != "E")
+        {
+            Console.WriteLine("Invalid choice");
+        }
+
+    } while (userInput != "S" && userInput != "A" && userInput != "R" && userInput != "E");
+
+    switch (userInput) 
+    {
+        case "S":
+            seeToDos(toDo);
+            break;
+        case "A":
+            addToDo(toDo);
+            break;
+        case "R":
+            
+            break;
+    }
+
+    
+
+} while (userInput != "E" && userInput != "e");
 
 
 
